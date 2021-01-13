9002e6e06830e4127715057a56c329230d090d65
Roman Voronov <roman.voronov@informicus.ru>
2021-01-13 21:10:37 +0700

    JwtBearer only

---
+++ Models/Credentials.cs
@@ -0,0 +1,8 @@
+namespace MyWebApi.Models
+{
+    public class Credentials
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+namespace MyWebApi.Models
+{
+    public class Person
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
